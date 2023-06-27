ac0c661cd2fb0fd977f6230667d44dc1fe83c304
MacBook <dk@nuts-agency.ru>
2023-06-27 17:03:52 +0300

    Initial Commit

---
+++ yandexmapsmobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yandexmapsmobile
+//
+//  Created by 1 on 27.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ yandexmapsmobile/yandexmapsmobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  yandexmapsmobileApp.swift
+//  yandexmapsmobile
+//
+//  Created by 1 on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct yandexmapsmobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
